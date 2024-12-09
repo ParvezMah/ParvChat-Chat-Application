27c32047a838102dddbb777e05673af2da7a4d98
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-12-10 00:56:55 +0600

    Database setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT;
 // Routes
 app.use("/api/auth", authRoutes);
 
 app.listen(5001, ()=>{
-    console.log("server is running");
+    console.log("server is running " + PORT);
+    connectDB();
 })
