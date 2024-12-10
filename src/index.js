8857bd655ca445113c25a0a5c9022c65a16841b8
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-12-11 04:43:04 +0600

    Messages Routes setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -15,6 +16,7 @@ app.use(cookieParser()); //to parse cookies attached to client requests, making 
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(5001, ()=>{
     console.log("server is running " + PORT);
